bf95879a07d5cdfda37b1f3510a06fd3527807fc
Automated Publisher <actions@users.noreply.github.com>
2021-11-02 18:42:03 +0000

    Automated publish: Tue Nov  2 18:42:03 UTC 2021 26c5d506a2e94d19d519e3e1d128d07af19c9ffd

--- build/ios-swift/StyleDictionaryColor.swift
+++ build/ios-swift/StyleDictionaryColor.swift
@@ -4,43 +4,43 @@
 //
 
 // Do not edit directly
-// Generated on Tue, 02 Nov 2021 18:39:12 GMT
+// Generated on Tue, 02 Nov 2021 18:42:02 GMT
 
 
 import UIKit
 
 public enum StyleDictionaryColor {
-    public static let colorAcaciasColorsBasicFailure = #d12a46ff
-    public static let colorAcaciasColorsBasicSandstone = #f5f3ebff
-    public static let colorAcaciasColorsBasicSuccess = #0f9451ff
-    public static let colorAcaciasColorsBasicWhite = #ffffffff
-    public static let colorAcaciasColorsFakeBold = #1f1f1fff
-    public static let colorAcaciasColorsGrape100 = #f4edf3ff
-    public static let colorAcaciasColorsGrape200 = #e3d2e0ff
-    public static let colorAcaciasColorsGrape300 = #d1b7cdff
-    public static let colorAcaciasColorsGrape400 = #c09dbaff
-    public static let colorAcaciasColorsGrape500 = #af82a8ff
-    public static let colorAcaciasColorsGrape600 = #9e6795ff
-    public static let colorAcaciasColorsGrape700 = #8d4c82ff
-    public static let colorAcaciasColorsGrey100 = #ecececff
-    public static let colorAcaciasColorsGrey200 = #cfcfcfff
-    public static let colorAcaciasColorsGrey300 = #b2b2b2ff
-    public static let colorAcaciasColorsGrey400 = #959595ff
-    public static let colorAcaciasColorsGrey500 = #787878ff
-    public static let colorAcaciasColorsGrey600 = #5b5b5bff
-    public static let colorAcaciasColorsGrey700 = #3e3e3eff
-    public static let colorAcaciasColorsOrange100 = #f8efe8ff
-    public static let colorAcaciasColorsOrange200 = #eed6c7ff
-    public static let colorAcaciasColorsOrange300 = #e4bda5ff
-    public static let colorAcaciasColorsOrange400 = #daa483ff
-    public static let colorAcaciasColorsOrange500 = #d08c61ff
-    public static let colorAcaciasColorsOrange600 = #c6733fff
-    public static let colorAcaciasColorsOrange700 = #bc5a1dff
-    public static let colorAcaciasColorsOxblood100 = #f2edecff
-    public static let colorAcaciasColorsOxblood200 = #dfd2cfff
-    public static let colorAcaciasColorsOxblood300 = #ccb6b3ff
-    public static let colorAcaciasColorsOxblood400 = #b99b96ff
-    public static let colorAcaciasColorsOxblood500 = #a68079ff
-    public static let colorAcaciasColorsOxblood600 = #93645dff
-    public static let colorAcaciasColorsOxblood700 = #804940ff
+    public static let acaciasColorsBasicFailure = UIColor(red: 0.820, green: 0.165, blue: 0.275, alpha: 1)
+    public static let acaciasColorsBasicSandstone = UIColor(red: 0.961, green: 0.953, blue: 0.922, alpha: 1)
+    public static let acaciasColorsBasicSuccess = UIColor(red: 0.059, green: 0.580, blue: 0.318, alpha: 1)
+    public static let acaciasColorsBasicWhite = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
+    public static let acaciasColorsFakeBold = UIColor(red: 0.122, green: 0.122, blue: 0.122, alpha: 1)
+    public static let acaciasColorsGrape100 = UIColor(red: 0.957, green: 0.929, blue: 0.953, alpha: 1)
+    public static let acaciasColorsGrape200 = UIColor(red: 0.890, green: 0.824, blue: 0.878, alpha: 1)
+    public static let acaciasColorsGrape300 = UIColor(red: 0.820, green: 0.718, blue: 0.804, alpha: 1)
+    public static let acaciasColorsGrape400 = UIColor(red: 0.753, green: 0.616, blue: 0.729, alpha: 1)
+    public static let acaciasColorsGrape500 = UIColor(red: 0.686, green: 0.510, blue: 0.659, alpha: 1)
+    public static let acaciasColorsGrape600 = UIColor(red: 0.620, green: 0.404, blue: 0.584, alpha: 1)
+    public static let acaciasColorsGrape700 = UIColor(red: 0.553, green: 0.298, blue: 0.510, alpha: 1)
+    public static let acaciasColorsGrey100 = UIColor(red: 0.925, green: 0.925, blue: 0.925, alpha: 1)
+    public static let acaciasColorsGrey200 = UIColor(red: 0.812, green: 0.812, blue: 0.812, alpha: 1)
+    public static let acaciasColorsGrey300 = UIColor(red: 0.698, green: 0.698, blue: 0.698, alpha: 1)
+    public static let acaciasColorsGrey400 = UIColor(red: 0.584, green: 0.584, blue: 0.584, alpha: 1)
+    public static let acaciasColorsGrey500 = UIColor(red: 0.471, green: 0.471, blue: 0.471, alpha: 1)
+    public static let acaciasColorsGrey600 = UIColor(red: 0.357, green: 0.357, blue: 0.357, alpha: 1)
+    public static let acaciasColorsGrey700 = UIColor(red: 0.243, green: 0.243, blue: 0.243, alpha: 1)
+    public static let acaciasColorsOrange100 = UIColor(red: 0.973, green: 0.937, blue: 0.910, alpha: 1)
+    public static let acaciasColorsOrange200 = UIColor(red: 0.933, green: 0.839, blue: 0.780, alpha: 1)
+    public static let acaciasColorsOrange300 = UIColor(red: 0.894, green: 0.741, blue: 0.647, alpha: 1)
+    public static let acaciasColorsOrange400 = UIColor(red: 0.855, green: 0.643, blue: 0.514, alpha: 1)
+    public static let acaciasColorsOrange500 = UIColor(red: 0.816, green: 0.549, blue: 0.380, alpha: 1)
+    public static let acaciasColorsOrange600 = UIColor(red: 0.776, green: 0.451, blue: 0.247, alpha: 1)
+    public static let acaciasColorsOrange700 = UIColor(red: 0.737, green: 0.353, blue: 0.114, alpha: 1)
+    public static let acaciasColorsOxblood100 = UIColor(red: 0.949, green: 0.929, blue: 0.925, alpha: 1)
+    public static let acaciasColorsOxblood200 = UIColor(red: 0.875, green: 0.824, blue: 0.812, alpha: 1)
+    public static let acaciasColorsOxblood300 = UIColor(red: 0.800, green: 0.714, blue: 0.702, alpha: 1)
+    public static let acaciasColorsOxblood400 = UIColor(red: 0.725, green: 0.608, blue: 0.588, alpha: 1)
+    public static let acaciasColorsOxblood500 = UIColor(red: 0.651, green: 0.502, blue: 0.475, alpha: 1)
+    public static let acaciasColorsOxblood600 = UIColor(red: 0.576, green: 0.392, blue: 0.365, alpha: 1)
+    public static let acaciasColorsOxblood700 = UIColor(red: 0.502, green: 0.286, blue: 0.251, alpha: 1)
 }
--- build/ios/StyleDictionaryColor.m
+++ build/ios/StyleDictionaryColor.m
@@ -4,7 +4,7 @@
 //
 
 // Do not edit directly
-// Generated on Tue, 02 Nov 2021 18:39:12 GMT
+// Generated on Tue, 02 Nov 2021 18:42:02 GMT
 
 
 #import "StyleDictionaryColor.h"
@@ -21,39 +21,39 @@
 
   dispatch_once(&onceToken, ^{
     colorArray = @[
-#1f1f1fff,
-#ecececff,
-#cfcfcfff,
-#b2b2b2ff,
-#959595ff,
-#787878ff,
-#5b5b5bff,
-#3e3e3eff,
-#f2edecff,
-#dfd2cfff,
-#ccb6b3ff,
-#b99b96ff,
-#a68079ff,
-#93645dff,
-#804940ff,
-#f4edf3ff,
-#e3d2e0ff,
-#d1b7cdff,
-#c09dbaff,
-#af82a8ff,
-#9e6795ff,
-#8d4c82ff,
-#f8efe8ff,
-#eed6c7ff,
-#e4bda5ff,
-#daa483ff,
-#d08c61ff,
-#c6733fff,
-#bc5a1dff,
-#ffffffff,
-#f5f3ebff,
-#0f9451ff,
-#d12a46ff
+[UIColor colorWithRed:0.122f green:0.122f blue:0.122f alpha:1.000f],
+[UIColor colorWithRed:0.925f green:0.925f blue:0.925f alpha:1.000f],
+[UIColor colorWithRed:0.812f green:0.812f blue:0.812f alpha:1.000f],
+[UIColor colorWithRed:0.698f green:0.698f blue:0.698f alpha:1.000f],
+[UIColor colorWithRed:0.584f green:0.584f blue:0.584f alpha:1.000f],
+[UIColor colorWithRed:0.471f green:0.471f blue:0.471f alpha:1.000f],
+[UIColor colorWithRed:0.357f green:0.357f blue:0.357f alpha:1.000f],
+[UIColor colorWithRed:0.243f green:0.243f blue:0.243f alpha:1.000f],
+[UIColor colorWithRed:0.949f green:0.929f blue:0.925f alpha:1.000f],
+[UIColor colorWithRed:0.875f green:0.824f blue:0.812f alpha:1.000f],
+[UIColor colorWithRed:0.800f green:0.714f blue:0.702f alpha:1.000f],
+[UIColor colorWithRed:0.725f green:0.608f blue:0.588f alpha:1.000f],
+[UIColor colorWithRed:0.651f green:0.502f blue:0.475f alpha:1.000f],
+[UIColor colorWithRed:0.576f green:0.392f blue:0.365f alpha:1.000f],
+[UIColor colorWithRed:0.502f green:0.286f blue:0.251f alpha:1.000f],
+[UIColor colorWithRed:0.957f green:0.929f blue:0.953f alpha:1.000f],
+[UIColor colorWithRed:0.890f green:0.824f blue:0.878f alpha:1.000f],
+[UIColor colorWithRed:0.820f green:0.718f blue:0.804f alpha:1.000f],
+[UIColor colorWithRed:0.753f green:0.616f blue:0.729f alpha:1.000f],
+[UIColor colorWithRed:0.686f green:0.510f blue:0.659f alpha:1.000f],
+[UIColor colorWithRed:0.620f green:0.404f blue:0.584f alpha:1.000f],
+[UIColor colorWithRed:0.553f green:0.298f blue:0.510f alpha:1.000f],
+[UIColor colorWithRed:0.973f green:0.937f blue:0.910f alpha:1.000f],
+[UIColor colorWithRed:0.933f green:0.839f blue:0.780f alpha:1.000f],
+[UIColor colorWithRed:0.894f green:0.741f blue:0.647f alpha:1.000f],
+[UIColor colorWithRed:0.855f green:0.643f blue:0.514f alpha:1.000f],
+[UIColor colorWithRed:0.816f green:0.549f blue:0.380f alpha:1.000f],
+[UIColor colorWithRed:0.776f green:0.451f blue:0.247f alpha:1.000f],
+[UIColor colorWithRed:0.737f green:0.353f blue:0.114f alpha:1.000f],
+[UIColor colorWithRed:1.000f green:1.000f blue:1.000f alpha:1.000f],
+[UIColor colorWithRed:0.961f green:0.953f blue:0.922f alpha:1.000f],
+[UIColor colorWithRed:0.059f green:0.580f blue:0.318f alpha:1.000f],
+[UIColor colorWithRed:0.820f green:0.165f blue:0.275f alpha:1.000f]
     ];
   });
 
