04bd31cc32eba4c35ba57d8da851df8fffe56633
Automated Publisher <actions@users.noreply.github.com>
2021-11-02 17:20:16 +0000

    Automated publish: Tue Nov  2 17:20:16 UTC 2021 590e8f32fdbf74fe53e87a13f045f2627f202859

--- build/ios-swift/StyleDictionaryColor.swift
+++ build/ios-swift/StyleDictionaryColor.swift
@@ -4,66 +4,43 @@
 //
 
 // Do not edit directly
-// Generated on Fri, 17 Sep 2021 10:23:13 GMT
+// Generated on Tue, 02 Nov 2021 17:20:16 GMT
 
 
 import UIKit
 
 public enum StyleDictionaryColor {
-    public static let colorsBackground = #ffffffff
-    public static let colorsBackground = #000000ff
-    public static let colorsBackgroundHighlight = #17181aff
-    public static let colorsBackgroundHighlight = #2c2e30ff
-    public static let colorsBackgroundVariant = #f1f2f4ff
-    public static let colorsBackgroundVariant = #222325ff
-    public static let colorsOnAlwaysWhiteHighEmphasis = #020203ff
-    public static let colorsOnAlwaysWhiteHighEmphasis = #020203ff
-    public static let colorsOnAlwaysWhiteLowEmphasis = #717579ff
-    public static let colorsOnAlwaysWhiteLowEmphasis = #717579ff
-    public static let colorsOnAlwaysWhiteMediumEmphasis = #515258ff
-    public static let colorsOnAlwaysWhiteMediumEmphasis = #515258ff
-    public static let colorsOnBackgroundHighEmphasis = #020203ff
-    public static let colorsOnBackgroundHighEmphasis = #f7f8f9ff
-    public static let colorsOnBackgroundHighlightHighEmphasis = #f7f8f9ff
-    public static let colorsOnBackgroundHighlightHighEmphasis = #f7f8f9ff
-    public static let colorsOnBackgroundHighlightLowEmphasis = #787f88ff
-    public static let colorsOnBackgroundHighlightLowEmphasis = #9197a1ff
-    public static let colorsOnBackgroundHighlightMediumEmphasis = #b2b4b9ff
-    public static let colorsOnBackgroundHighlightMediumEmphasis = #afb0b6ff
-    public static let colorsOnBackgroundLowEmphasis = #6a6d71ff
-    public static let colorsOnBackgroundLowEmphasis = #848a95ff
-    public static let colorsOnBackgroundMediumEmphasis = #515258ff
-    public static let colorsOnBackgroundMediumEmphasis = #afb0b6ff
-    public static let colorsOnUiHighEmphasis = #020203ff
-    public static let colorsOnUiHighEmphasis = #f7f8f9ff
-    public static let colorsOnUiHighlightHighEmphasis = #f7f8f9ff
-    public static let colorsOnUiHighlightHighEmphasis = #020203ff
-    public static let colorsOnUiHighlightLowEmphasis = #787f88ff
-    public static let colorsOnUiHighlightLowEmphasis = #717579ff
-    public static let colorsOnUiHighlightMediumEmphasis = #b2b4b9ff
-    public static let colorsOnUiHighlightMediumEmphasis = #515258ff
-    public static let colorsOnUiLowEmphasis = #676a6eff
-    public static let colorsOnUiLowEmphasis = #878991ff
-    public static let colorsOnUiMediumEmphasis = #515258ff
-    public static let colorsOnUiMediumEmphasis = #bfc2c8ff
-    public static let colorsSemanticError = #d4000dff
-    public static let colorsSemanticError = #d4000dff
-    public static let colorsSemanticSuccess = #00af2cff
-    public static let colorsSemanticSuccess = #00af2cff
-    public static let colorsSemanticWarning = #ff8a00ff
-    public static let colorsSemanticWarning = #ff8a00ff
-    public static let colorsSpecialAlwaysDarkOverlay = #000000cc
-    public static let colorsSpecialAlwaysDarkOverlay = #000000cc
-    public static let colorsSpecialAlwaysWhite = #ffffffff
-    public static let colorsSpecialAlwaysWhite = #ffffffff
-    public static let colorsUiHighContrast = #a8aaafff
-    public static let colorsUiHighContrast = #6e7175ff
-    public static let colorsUiHighlight = #17181aff
-    public static let colorsUiHighlight = #fdfeffff
-    public static let colorsUiLowContrast = #f1f2f4ff
-    public static let colorsUiLowContrast = #222325ff
-    public static let colorsUiMediumContrast = #d8daddff
-    public static let colorsUiMediumContrast = #424447ff
-    public static let colorsUiNormalContrast = #c0c2c6ff
-    public static let colorsUiNormalContrast = #4d4f53ff
+    public static let acaciaBasicFailure = UIColor(red: 0.820, green: 0.165, blue: 0.275, alpha: 1)
+    public static let acaciaBasicSandstone = UIColor(red: 0.961, green: 0.953, blue: 0.922, alpha: 1)
+    public static let acaciaBasicSuccess = UIColor(red: 0.059, green: 0.580, blue: 0.318, alpha: 1)
+    public static let acaciaBasicWhite = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
+    public static let acaciaGrape100 = UIColor(red: 0.957, green: 0.929, blue: 0.953, alpha: 1)
+    public static let acaciaGrape200 = UIColor(red: 0.890, green: 0.824, blue: 0.878, alpha: 1)
+    public static let acaciaGrape300 = UIColor(red: 0.820, green: 0.718, blue: 0.804, alpha: 1)
+    public static let acaciaGrape400 = UIColor(red: 0.753, green: 0.616, blue: 0.729, alpha: 1)
+    public static let acaciaGrape500 = UIColor(red: 0.686, green: 0.510, blue: 0.659, alpha: 1)
+    public static let acaciaGrape600 = UIColor(red: 0.620, green: 0.404, blue: 0.584, alpha: 1)
+    public static let acaciaGrape700 = UIColor(red: 0.553, green: 0.298, blue: 0.510, alpha: 1)
+    public static let acaciaGrey100 = UIColor(red: 0.925, green: 0.925, blue: 0.925, alpha: 1)
+    public static let acaciaGrey200 = UIColor(red: 0.812, green: 0.812, blue: 0.812, alpha: 1)
+    public static let acaciaGrey300 = UIColor(red: 0.698, green: 0.698, blue: 0.698, alpha: 1)
+    public static let acaciaGrey400 = UIColor(red: 0.584, green: 0.584, blue: 0.584, alpha: 1)
+    public static let acaciaGrey500 = UIColor(red: 0.471, green: 0.471, blue: 0.471, alpha: 1)
+    public static let acaciaGrey600 = UIColor(red: 0.357, green: 0.357, blue: 0.357, alpha: 1)
+    public static let acaciaGrey700 = UIColor(red: 0.243, green: 0.243, blue: 0.243, alpha: 1)
+    public static let acaciaOrange100 = UIColor(red: 0.973, green: 0.937, blue: 0.910, alpha: 1)
+    public static let acaciaOrange200 = UIColor(red: 0.933, green: 0.839, blue: 0.780, alpha: 1)
+    public static let acaciaOrange300 = UIColor(red: 0.894, green: 0.741, blue: 0.647, alpha: 1)
+    public static let acaciaOrange400 = UIColor(red: 0.855, green: 0.643, blue: 0.514, alpha: 1)
+    public static let acaciaOrange500 = UIColor(red: 0.816, green: 0.549, blue: 0.380, alpha: 1)
+    public static let acaciaOrange600 = UIColor(red: 0.776, green: 0.451, blue: 0.247, alpha: 1)
+    public static let acaciaOrange700 = UIColor(red: 0.737, green: 0.353, blue: 0.114, alpha: 1)
+    public static let acaciaOxblood100 = UIColor(red: 0.949, green: 0.929, blue: 0.925, alpha: 1)
+    public static let acaciaOxblood200 = UIColor(red: 0.875, green: 0.824, blue: 0.812, alpha: 1)
+    public static let acaciaOxblood300 = UIColor(red: 0.800, green: 0.714, blue: 0.702, alpha: 1)
+    public static let acaciaOxblood400 = UIColor(red: 0.725, green: 0.608, blue: 0.588, alpha: 1)
+    public static let acaciaOxblood500 = UIColor(red: 0.651, green: 0.502, blue: 0.475, alpha: 1)
+    public static let acaciaOxblood600 = UIColor(red: 0.576, green: 0.392, blue: 0.365, alpha: 1)
+    public static let acaciaOxblood700 = UIColor(red: 0.502, green: 0.286, blue: 0.251, alpha: 1)
+    public static let fakeBold = UIColor(red: 0.122, green: 0.122, blue: 0.122, alpha: 1)
 }
